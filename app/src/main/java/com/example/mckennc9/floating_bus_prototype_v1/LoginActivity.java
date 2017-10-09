c7402f22d155270d83f56b10054b99a7f8465f06
mckennc9 <mckennc9@tcd.ie>
2017-10-09 12:53:46 +0100

    Adding Login features
    Added new login activity screen, starting to add functionality

---
+++ app/src/main/java/com/example/mckennc9/floating_bus_prototype_v1/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.mckennc9.floating_bus_prototype_v1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mckennc9 on 10/9/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceSate){
+        super.onCreate(savedInstanceSate);
+    }
+
+}
